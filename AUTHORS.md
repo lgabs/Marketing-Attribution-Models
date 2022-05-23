d234b61745b293ae8fb4c3f9ea7adbdd48715662
Luan Gabriel Silva Fernandes <luan.fernandes@buser.com.br>
2022-05-23 16:11:48 -0300

    update license and authors

---
+++ AUTHORS.md
@@ -0,0 +1,10 @@
+# This is the list of Marketing Attribution Models' significant contributors.
+#
+# This does not necessarily list everyone who has contributed code,
+# especially since many employees of one corporation may be contributing.
+# To see the full list of contributors, see the revision history in
+# source control.
+Digital Inc Tecnologia e Marketing SA
+Andre Tocci (andre.tocci@dp6.com.br)
+Buser Brasil
+Luan Fernandes (luan.fernandes@buser.com.br)
--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ with open("README.md", "r") as f:
 setup(
     name="marketing_attribution_models",
     version="1.0.8",
-    description="Metodos de atribuicao de midia",
+    description="Library to calculate Marketing Attributions using several models. Originally distributed by Andre Tocci (andre.tocci@dp6.com.br) and adapted by Luan Fernandes.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author="Andre Tocci",
-    author_email="andre.tocci@dp6.com.br",
+    author="Luan Fernandes",
+    author_email="luan.fernades@buser.com.br",
     url="https://github.com/DP6/Marketing-Attribution-Models",
     packages=["marketing_attribution_models"],
     install_requires=[
